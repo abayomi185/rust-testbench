65a446960fe5bd975f7505e4b5085ed6f6953224
Yomi Ikuru <abayomi.ikuru@gmail.com>
2022-11-10 21:26:50 +0000

    Realise that Tokio had breaking change to majorversion 1

--- tokio-test/src/main.rs
+++ tokio-test/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use futures::prelude::*;
+
+async fn app() {
+    // let mut stream = tokio::net::TcpStream::connect("
+}
+
+#[tokio::main]
+async fn main() {
+    // println!("Hello, world!");
+    todo!()
 }
