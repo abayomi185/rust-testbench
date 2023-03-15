f06c691ec1bf0602d30d8f54ee9ab8a4c9bd3f2a
Yomi Ikuru <captyomjnr@gmail.com>
2023-03-15 11:03:45 +0000

    Update csv test using polars

--- csv-test/src/main.rs
+++ csv-test/src/main.rs
@@ -1,6 +1,10 @@
 use anyhow::Result;
 use polars::prelude::*;
 use serde_json;
+use std::env;
+
+#[macro_use]
+extern crate fstrings;
 
 const DATAFILE: &str = "data_large.csv";
 
@@ -27,6 +31,7 @@ fn roundTo1dpString(number: f64) -> String {
 }
 
 fn load_csv(file_path: &str) -> Result<DataFrame> {
+    println_f!("Loading CSV file: {file_path}");
     let df = CsvReader::from_path(file_path)?
         .infer_schema(Some(100))
         .has_header(true)
@@ -34,16 +39,29 @@ fn load_csv(file_path: &str) -> Result<DataFrame> {
     Ok(df)
 }
 
-fn applyCurrencyConversionAndColFiltering() {}
+fn apply_currency_conversion_and_col_filtering(
+    df: &DataFrame,
+    inflation_factor: f64,
+    exchange_rate: f64,
+    currency_code: &str,
+    columns_to_remove: Vec<&str>,
+    start_year_for_granular_data: u32,
+    start_month_for_granular_data: u8,
+) {
+    // df.filter(df.column("Time (UTC)").dt().year()? > );
+    // .drop_in_place(columns_to_remove)
+    // .unwrap();
+
+    // df.filter(df.column("Time (UTC)").dt().year()? > start_year_for_granular_data)
+    //     .drop_in_place(columns_to_remove)
+    //     .unwrap();
+
+    df.drop_many(&columns_to_remove);
+}
 
 fn main() {
     // type CustomTypeAlias = i32;
     // let random_number: CustomTypeAlias = 10;
-
-    // let mut dict = HashMap::new();
-    // dict.insert(1, "one");
-
-    // println!("{:#?}", dict);
 
     // let value_to_get = 1;
     // // Output is option<&str> - Some value or None
@@ -62,8 +80,32 @@ fn main() {
     //     println!("Error parsing number");
     // }
 
-    let mut df = load_csv(format!("data/{DATAFILE}")).unwrap();
+    let mut df = load_csv(format!("data/{DATAFILE}").as_str()).unwrap();
 
-    let mut file = std::fs::File::create("out/{DATAFILE}").unwrap();
+    println!("{:#?}", df);
+
+    const INFLATION_FACTOR: f64 = 1.0;
+    const EXCHANGE_RATE: f64 = 1.0;
+    const CURRENCY_CODE: &str = "GBP";
+    const COLUMNS_TO_REMOVE: Vec<&str> = vec![];
+    const START_YEAR_FOR_GRANULAR_DATA: u32 = 2020;
+    const START_MONTH_FOR_GRANULAR_DATA: u8 = 0;
+
+    use std::time::Instant;
+    let now = Instant::now();
+    apply_currency_conversion_and_col_filtering(
+        &df,
+        INFLATION_FACTOR,
+        EXCHANGE_RATE,
+        CURRENCY_CODE,
+        COLUMNS_TO_REMOVE,
+        START_YEAR_FOR_GRANULAR_DATA,
+        START_MONTH_FOR_GRANULAR_DATA,
+    );
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
+
+    let mut file = std::fs::File::create(format!("out/{DATAFILE}")).unwrap();
     CsvWriter::new(&mut file).finish(&mut df).unwrap();
 }
